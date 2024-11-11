2de67b2d7cfa1fca927d43e82f19f958b6d9c86b
Lucas Hemmingsen <99349701+LucasHemm@users.noreply.github.com>
2024-11-11 12:20:57 +0100

    US-14

---
+++ CustomerService/CustomerService/API/CustomerAPI.cs
@@ -0,0 +1,33 @@
+﻿using CustomerService.DTOs;
+using CustomerService.Facades;
+using CustomerService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerService.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomerApi : ControllerBase
+{
+    private readonly CustomerFacade _customerFacade;
+
+    public CustomerApi(CustomerFacade customerFacade)
+    {
+        _customerFacade = customerFacade;
+    }
+    
+    [HttpPost]
+    public IActionResult CreateRestaurant([FromBody] CustomerDTO customerDto)
+    {
+        try
+        {
+            _customerFacade.CreateCustomer(customerDto);
+            return Ok("Customer created successfully");
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+}
